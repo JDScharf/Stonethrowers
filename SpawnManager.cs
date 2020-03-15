b7758e5908422b23f8814537be5ab6ea880dc1a5
JDScharf <43705075+JDScharf@users.noreply.github.com>
2020-03-15 00:29:00 -0400

    Add files via upload

---
+++ SpawnManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject bird;
+    public GameObject policeOfficer;
+    public GameObject portalStart;
+    public GameObject portalEnd;
+
+    private float xPosition = 0;
+    private float zPositionBird = -15f;
+
+    private float birdsStartTime = 1;
+    private float birdsRepeatTime = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //SpawnPoliceOfficer();
+        // InvokeRepeating("SpawnRandomBirdPosition", birdsStartTime, birdsRepeatTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 
+    }
+
+    void SpawnRandomBirdPosition()
+    {
+        float randomY = Random.Range(5, 10f);
+        Vector3 spawnPosBird = new Vector3(xPosition, randomY, zPositionBird);
+        Instantiate(bird, spawnPosBird, bird.transform.rotation);
+    }
+}
